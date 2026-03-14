9711c04a9fe1afa7c3eb54e7aca7cd9e5b1886e1
Artur <arturmotuz2005@gmail.com>
2026-03-14 20:54:01 +0100

    Update weather_cli.py

--- weather_cli.py
+++ weather_cli.py
@@ -2,20 +2,34 @@ import requests
 
 API_KEY = "9482209c8a6d66f9fcf7a677f674c139"
 
-city = input("Enter city name: ")
 
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+def get_weather(city):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
 
-response = requests.get(url)
-data = response.json()
+    try:
+        response = requests.get(url)
+        data = response.json()
 
-if data["cod"] == 200:
-    temperature = data["main"]["temp"]
-    weather = data["weather"][0]["description"]
+        if data["cod"] == 200:
+            temperature = data["main"]["temp"]
+            weather = data["weather"][0]["description"]
 
-    print(f"\nWeather in {city}")
-    print("Temperature:", temperature, "°C")
-    print("Condition:", weather)
+            print(f"\nWeather in {city}")
+            print("Temperature:", temperature, "°C")
+            print("Condition:", weather)
 
-else:
-    print("City not found.")
+        else:
+            print("City not found.")
+
+    except Exception as e:
+        print("Error while fetching weather data:", e)
+
+
+def main():
+    print("=== Weather CLI ===")
+    city = input("Enter city name: ")
+    get_weather(city)
+
+
+if __name__ == "__main__":
+    main()
