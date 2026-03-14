31aa5222169db3258b8fcb56dfb0d20fb524c062
Artur <arturmotuz2005@gmail.com>
2026-03-14 20:42:20 +0100

    Create weather_cli.py

---
+++ weather_cli.py
@@ -0,0 +1,21 @@
+import requests
+
+API_KEY = "YOUR_API_KEY"
+
+city = input("Enter city name: ")
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+if data["cod"] == 200:
+    temperature = data["main"]["temp"]
+    weather = data["weather"][0]["description"]
+
+    print(f"\nWeather in {city}")
+    print("Temperature:", temperature, "°C")
+    print("Condition:", weather)
+
+else:
+    print("City not found.")
